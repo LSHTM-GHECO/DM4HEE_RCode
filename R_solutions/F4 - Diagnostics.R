173583285b0bea1e85a77b3d8d778a7d853c56f0
Jack Williams <jack.williams@lshtm.ac.uk>
2021-04-15 11:27:35 +0100

    Edits to Markdown exercise and R script (names etc.) - Unfinished

--- R_solutions/F4 - Diagnostics.R
+++ R_solutions/F4 - Diagnostics.R
@@ -2,27 +2,25 @@
 
 
 
-
 #### Model Parameters #### 
 
 lambda <- 30000 # This is our willingness to pay threshold
 prevalence <- 0.3
 
-# payoffs and consequences 
+# Payoffs and cost parameters for sick and healthy individuals, treated and not treated 
 
 outcome.names <- c("Sick per treated", "Sick person not treated", 
                    "Healthy person treated", "Health person not treated")
-
 expected.cost <- c(6000, 5000, 3000, 1000)
-expected.QALY <- c(0.8, 0.5, 0.95, 1)
+expected.qaly <- c(0.8, 0.5, 0.95, 1)
 
-expected.values <- data.frame(outcome.names, expected.cost, expected.QALY,
-                              NMB = NA)
+parameter.values <- data.frame(outcome.names, expected.cost, expected.qaly,
+                              nmb = NA)
 
-expected.values$NMB <- (expected.QALY * lambda) - expected.cost
+parameter.values$nmb <- (expected.qaly * lambda) - expected.cost
 
 # here you can see the expected costs, QALYs and NMB for each possible outcome
-expected.values
+parameter.values
 
 
 # Estimating costs and QALYs associated with treating all vs. treat none 
@@ -30,34 +28,33 @@ expected.values
 treat.cost <- prevalence * expected.cost[1] + (1 - prevalence) * expected.cost[3]
 notreat.cost <- prevalence * expected.cost[2] + (1 - prevalence) * expected.cost[4]
 
-treat.QALY <- prevalence * expected.QALY[1] + (1 - prevalence) * expected.QALY[3]
-notreat.QALY <- prevalence * expected.QALY[2] + (1 - prevalence) * expected.QALY[4]
+treat.qaly <- prevalence * expected.qaly[1] + (1 - prevalence) * expected.qaly[3]
+notreat.qaly <- prevalence * expected.qaly[2] + (1 - prevalence) * expected.qaly[4]
 
-treat.payoff <- data.frame(strategy =  c("Treat all", "Treat none"),
+expected.values <- data.frame(strategy =  c("Treat all", "Treat none"),
                            expected.cost = c(treat.cost, notreat.cost), 
-                           expected.QALY = c(treat.QALY, notreat.QALY), 
-                           NMB = NA)
+                           expected.qaly = c(treat.qaly, notreat.qaly), 
+                           nmb = NA)
 
-treat.payoff$NMB <- (treat.payoff$expected.QALY * lambda) - treat.payoff$expected.cost 
+expected.values$nmb <- (expected.values$expected.qaly * lambda) - expected.values$expected.cost 
 
-treat.payoff
+# You can view the expected cost, QALY and NMB values at the prevalence defined
+expected.values
 
 
-
-## Table 3 / 
 
 # Evaluate NMB across a range of prevalence values
 
-## We can create a function to calcualte NMB from the costs and QALYs of sick and healthy patients with prevalence  
-est.NMB <- function(prev, parameters = expected.values, lam = lambda){
+## We can create a function to calcualte NMB for sick and healthy patients (across prevalence values)  
+est.nmb <- function(prev, parameters = parameter.values, lam = lambda){
   
   # Treatment - look up correct costs and outcomes for treated
 
   cost.sick <-  parameters$expected.cost[1] 
   cost.healthy <-  parameters$expected.cost[3] 
   
-  qaly.sick <-  parameters$expected.QALY[1] 
-  qaly.healthy <-  parameters$expected.QALY[3] 
+  qaly.sick <-  parameters$expected.qaly[1] 
+  qaly.healthy <-  parameters$expected.qaly[3] 
   
   cost <- prev * cost.sick + (1-prev) * cost.healthy
   qaly <- prev * qaly.sick + (1-prev) * qaly.healthy
@@ -69,8 +66,8 @@ est.NMB <- function(prev, parameters = expected.values, lam = lambda){
   cost.sick <-  parameters$expected.cost[2] 
   cost.healthy <-  parameters$expected.cost[4] 
   
-  qaly.sick <-  parameters$expected.QALY[2] 
-  qaly.healthy <-  parameters$expected.QALY[4] 
+  qaly.sick <-  parameters$expected.qaly[2] 
+  qaly.healthy <-  parameters$expected.qaly[4] 
   
   cost <- prev * cost.sick + (1-prev) * cost.healthy
   qaly <- prev * qaly.sick + (1-prev) * qaly.healthy
@@ -78,30 +75,37 @@ est.NMB <- function(prev, parameters = expected.values, lam = lambda){
   nmb.notreat <- (qaly * lam) - cost
   
   return(data.frame(prevalence = prev, 
-                    NMB.treat = nmb.treat, 
-                    NMB.notreat = nmb.notreat))
+                    nmb.treat = nmb.treat, 
+                    nmb.notreat = nmb.notreat))
 }
 
 
-## We can now estimate the NMB of treating or not treating at any prevalence
-est.NMB(prev = 0.1)
-
+## We can now estimate the NMB of treating or not treating at any prevalence 
 # At a low prevalence (10%), NMB is higher for not treating
 # But at higher prevalence NMB is higher for treating everyone
-est.NMB(prev = 0.4)
+est.nmb(prev = 0.1)
+est.nmb(prev = 0.4)
 
 
-# We can also pass a vector of prevalence values into the function
-
+# Now pass a vector of prevalence values into the function
 prevalence.vector <- seq(from = 0, to = 1, by = 0.05)
-est.NMB(prev = prevalence.vector)
 
-## Expected value of a perfect test
+est.nmb(prev = prevalence.vector)
+
+# Save this output, so that we can create a plot
+no.test.nmb <- est.nmb(prev = prevalence.vector)
+
+
+
+### PLOT ? #### 
+
+
+#### Expected value of a perfect test ####
 
 # In this example, we will assume that all sick patients are treated, and all healthy patients are not
 
 
-est.NMB.perfect.test <- function(prev, parameters = expected.values, lam = lambda){
+est.nmb.perfect.test <- function(prev, parameters = parameter.values, lam = lambda){
   
   # Perfect test means sick is treated (i.e. row value 1 for expected values table)
   # whilst healthy is not treated (i.e. row value 4 from expected values table)
@@ -110,8 +114,8 @@ est.NMB.perfect.test <- function(prev, parameters = expected.values, lam = lambd
   cost.sick <-  parameters$expected.cost[1] 
   cost.healthy <-  parameters$expected.cost[4] 
   
-  qaly.sick <-  parameters$expected.QALY[1] 
-  qaly.healthy <-  parameters$expected.QALY[4] 
+  qaly.sick <-  parameters$expected.qaly[1] 
+  qaly.healthy <-  parameters$expected.qaly[4] 
   
   cost <- prev * cost.sick + (1-prev) * cost.healthy
   qaly <- prev * qaly.sick + (1-prev) * qaly.healthy
@@ -120,46 +124,45 @@ est.NMB.perfect.test <- function(prev, parameters = expected.values, lam = lambd
   
 
   return(data.frame(prevalence = prev, 
-                    NMB.perfect.test = nmb.perfect))
+                    nmb = nmb.perfect))
 }
 
-est.NMB.perfect.test(prev = 0.1)
-est.NMB.perfect.test(prev = prevalence.vector)
+est.nmb.perfect.test(prev = 0.1)
+est.nmb.perfect.test(prev = prevalence.vector)
+
+perfect.test.nmb <- est.nmb.perfect.test(prev = prevalence.vector)
 
 
-### Expected value of perfect diagnostic information (EVPDI) 
+# Estimate the Expected Value of Perfect Diagnostic Information (EVPDI) 
 
 # The EVPDI is the difference between the expected value of a perfect test and 
 # the expected value of the optimal course of action (treat All or treat None) in the absence of a test.
 
+# First, look again at the results of the NMB with no test available 
+no.test.nmb
 
-
-## We will do this for all prevalence values
-
-
-
-perfect.test <- est.NMB.perfect.test(prev = prevalence.vector)
-
-# First, look again at the results of the NMB with no test available (we can call this current)
-current <- est.NMB(prev = prevalence.vector)
-
-# now we can use the apply function to derive the maximum NMB from each strategy, and add to current table
-current$NMB.max <- apply(current[,2:3], 1, max)
+# Now we can use the apply function to derive the maximum NMB from each strategy, and add to current table
+no.test.nmb$nmb.max <- apply(no.test.nmb[,2:3], 1, max)
 
 ## Now take a look at the current table, displaying the maximum NMB for each prevalence (without a test)
-current
+# We can compare this to the NMB with a perfect test
+
+no.test.nmb
+perfect.test.nmb
 
 
 ## We can now compare this to the values expected from a perfect test
-evpdi <- perfect.test$NMB.perfect.test - current$NMB.max
+evpdi <- perfect.test.nmb$nmb - no.test.nmb$nmb.max
 
-evdpi.table <- data.frame(prevalence = prevalence.vector, 
-                          evpdi = evpdi)
+evpdi.table <- data.frame(prevalence = prevalence.vector, evpdi = evpdi)
 
-evdpi.table
+# Here is the table EVPDI
+evpdi.table
 
 
-### Expected Value of Clinical Information (EVCI) from an (imperfect) diagnostic test
+## PLOT? ## 
+
+#### Expected Value of Clinical Information (EVCI) from an (imperfect) diagnostic test ####
 
 test.char <- c(positive.mean = 1, positive.sd = 1, 
                negative.mean = -1, negative.sd = 1)
@@ -192,38 +195,38 @@ true.neg <- (1 - prevalence) * (1 - FPR)
 
 # Table showing diagnostic accuracy 
 diag.accuracy <- data.frame(diagnostic.threshold, true.pos, false.neg, false.pos, true.neg,
-                            NMB.test = 0)
+                            nmb.test = 0)
 
 
 # Estimate the NMB as a function of the diagnostic accuracy 
 
-NMB.test.vec <- rep(0, length(diagnostic.threshold))
+nmb.test.vec <- rep(0, length(diagnostic.threshold))
 
 for(i in 1:length(diagnostic.threshold)){
-  NMB.test.vec[i] <- sum(diag.accuracy[i,2:5] * expected.values$NMB)
+  nmb.test.vec[i] <- sum(diag.accuracy[i,2:5] * parameter.values$nmb)
 }  
 
 ## Note: An alternative method is to write a function into apply      
-NMB.test.vec <- apply(diag.accuracy[,2:5], 1, function(x) sum(x * expected.values$NMB))
+nmb.test.vec <- apply(diag.accuracy[,2:5], 1, function(x) sum(x * parameter.values$nmb))
 
 
 # We can view the NMB here
-data.frame(diagnostic.threshold, NMB.test.vec)    
+data.frame(diagnostic.threshold, nmb.test.vec)    
 
 
 
-diag.accuracy$NMB <- NMB.test
+diag.accuracy$nmb <- nmb.test
 
 
 # Calculate the EVCI (?)
 
 # Now we want to calcualte the EVCI across a range of prevalence values
 
-nmb.no.test <- est.NMB(prev = prevalence.vector)
+no.test.nmb <- est.nmb(prev = prevalence.vector)
 
 
 est.EVCI <- function(prev, diag = diagnostic.threshold, test = test.char, 
-                     nmb.input = expected.values$NMB, nmb.notest = nmb.no.test){
+                     nmb.input = parameter.values$NMB, nmb.notest = no.test.nmb){
 
   # true and false positive rate
   TPR <- 1 - pnorm(diag, test[1], test[2])
